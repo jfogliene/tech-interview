5305d9fb1da6847ce1aab12891f578105c15a1f0
Joao Fogliene <jfogliene@bol.com.br>
2020-09-03 23:30:48 -0300

    New solutions

--- src/com/fogliene/Main20200830.java
+++ src/com/fogliene/Main20200830.java
@@ -1,4 +1,46 @@
 package com.fogliene;
 
+/*
+Hi, here's your problem today. This problem was recently asked by Microsoft:
+
+You are given an array of integers in an arbitrary order. Return whether or not it is possible to make the array non-decreasing by modifying at most 1 element to any value.
+
+We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+
+Example:
+
+[13, 4, 7] should return true, since we can modify 13 to any value 4 or less, to make it non-decreasing.
+
+[13, 4, 1] however, should return false, since there is no way to modify just one element to make the array non-decreasing.
+
+Here is the function signature:
+
+def check(lst):
+  # Fill this in.
+
+print check([13, 4, 7])
+# True
+print check([5,1,3,2,5])
+# False
+
+Can you find a solution in O(n) time?
+ */
 public class Main20200830 {
+    public static void main(String[] args) {
+        System.out.println(check(new Integer[] { 13, 4, 7 })); // true
+        System.out.println(check(new Integer[] { 13, 4, 1 })); // false
+        System.out.println(check(new Integer[] { 5, 1, 3, 2, 5 })); // false
+    }
+
+    public static boolean check(Integer[] arr) {
+        Integer count = 0;
+
+        for (int i = 0; i < arr.length -1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                if (++count > 1) return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/com/fogliene/Main20200831.java
+++ src/com/fogliene/Main20200831.java
@@ -1,4 +1,109 @@
 package com.fogliene;
 
+/*
+Hi, here's your problem today. This problem was recently asked by Apple:
+
+Given an integer k and a binary search tree, find the floor (less than or equal to) of k, and the ceiling (larger than or equal to) of k. If either does not exist, then print them as None.
+
+Here is the definition of a node for the tree.
+
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+
+def findCeilingFloor(root_node, k, floor=None, ceil=None):
+  # Fill this in.
+
+root = Node(8)
+root.left = Node(4)
+root.right = Node(12)
+
+root.left.left = Node(2)
+root.left.right = Node(6)
+
+root.right.left = Node(10)
+root.right.right = Node(14)
+
+print findCeilingFloor(root, 5)
+# (4, 6)
+ */
 public class Main20200831 {
+    public static void main(String[] args) {
+        Node root = new Node(8);
+        root.left = new Node(4);
+        root.right = new Node(12);
+
+        root.left.left = new Node(2);
+        root.left.right = new Node(6);
+
+        root.right.left = new Node(10);
+        root.right.right = new Node(14);
+
+        Integer[] result = findCeilingFloor(root, 5);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+
+    public static Integer[] findCeilingFloor(Node root, Integer value) {
+        Integer floor = findFloor(root, value);
+        Integer ceiling = findCeiling(root, value);
+
+        return new Integer[] { floor, ceiling };
+    }
+
+    public static Integer findFloor(Node root, Integer value) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.value == value) {
+            return value;
+        }
+
+        if (root.value < value) {
+            Integer k = findFloor(root.right, value);
+
+            if (k == null) {
+                return root.value;
+            } else {
+                return k;
+            }
+        } else {
+            return findFloor(root.left, value);
+        }
+    }
+
+
+    public static Integer findCeiling(Node root, Integer value) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.value == value) {
+            return value;
+        }
+
+        if (root.value > value) {
+            Integer k = findCeiling(root.left, value);
+
+            if (k == null) {
+                return root.value;
+            } else {
+                return k;
+            }
+        } else {
+            return findCeiling(root.right, value);
+        }
+    }
+}
+
+class Node {
+    public Node left;
+    public Node right;
+    public Integer value;
+
+    public Node(Integer value) {
+        this.value = value;
+    }
 }
